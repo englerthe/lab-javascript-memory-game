da1cce04ed22312d6e67de03992c8f9faf897c45
Englert <englert@media-saturn.com>
2019-09-03 16:42:16 +0200

    final version

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -16,9 +16,11 @@ class MemoryGame {
   }
   checkIfPair(card1, card2) {
     this.pairsClicked +=1;
+    document.getElementById("pairs_clicked").innerHTML = this.pairsClicked;
     this.pickedCards = [card1, card2];
     if (this.pickedCards[0].getAttribute("name") === this.pickedCards[1].getAttribute("name")) {
       this.pairsGuessed +=1;
+      document.getElementById("pairs_guessed").innerHTML = this.pairsGuessed;
       return true;
     }
     return false;
@@ -29,34 +31,40 @@ class MemoryGame {
       return false;
     } else if (this.pairsGuessed < this.cards.length/2) {
       return false;
-    } 
-    return true;
+    }
+    document.querySelector('#finished').innerHTML = "Du hast gewonnen !";
+    // location.reload();
+    // return true;
   }
 
   cardClicked(card) {
+    if (this.pickedCards.length == 2) {
+      return;
+    }
     card.className = "front";
     card.parentNode.childNodes[1].className = "back";
     this.pickedCards.push(card); 
     if (this.pickedCards.length == 2) {
       if (this.checkIfPair(this.pickedCards[0], this.pickedCards[1]) == false ) {
-      console.log(this.pickedCards);  
+      // console.log(this.pickedCards);  
         this.flipCardsBack();
-        //this.performTimeout();
-      } else {  
-      this.pickedCards = [];
-      console.log(this.pairsGuessed);  
+      } else {
+        this.isFinished();
+        this.pickedCards = [];
+      // console.log(this.pairsGuessed);  
       }
     }
   }
 
   flipCardsBack() {
     setTimeout( () => {
-    console.log(this.pickedCards);
+    // console.log(this.pickedCards);
     this.pickedCards[0].className = "back";
     this.pickedCards[0].parentNode.childNodes[1].className = "front";
     this.pickedCards[1].className = "back";
     this.pickedCards[1].parentNode.childNodes[1].className = "front";
     this.pickedCards = [];
-    }, 1500)
+    }, 20)
   } 
+
 };
